9dd675e8766dc25382e4d4b882244717ac2337a7
Ezra Ju <96998749+EzraJJu@users.noreply.github.com>
2024-03-13 12:55:20 -0400

    Intake subsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,8 +1,8 @@
 package frc.robot.subsystems; 
 
-import com.revrobotics.CANSparkMax; 
-import com.revrobotics.RelativeEncoder; 
-import com.revrobotics.SparkPIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterConstants;
+import edu.wpi.first.wpilibj.Relay;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANSparkMax.ControlType;
@@ -15,8 +15,21 @@ import frc.robot.Constants.ShooterConstants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
-    public IntakeSubsystem() {
+    private final TaleonSRX m_motor = new TalonSRX(ShooterConstants);
+    private final Relay m_relay = new Relay(ShooterConstants.kRelayPort);
 
+
+
+    public IntakeSubsystem() {
+        m_motor.configFactoryDefault(); 
+        m_motor.setInverted(ShooterConstants.kMotor); //change probably not right :P
+    }
+
+    public void periodic() {
+    }
+    
+    public void setSpeed(double speed) {
+        m_motor.set(speed);
     }
 
 
