fd42a1de1844118c31431273c1a6593637486b88
KalenShamy <kalen.shamy@gmail.com>
2024-03-15 08:48:59 -0400

    fix armfeedforward for encoder

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -5,13 +5,9 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder; 
 import com.revrobotics.SparkPIDController;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.math.controller.ArmFeedforward;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
-
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
 
 import frc.robot.Constants.ArmConstants;
 
@@ -22,7 +18,7 @@ public class ArmSubsystem extends SubsystemBase {
 
     private final RelativeEncoder m_encoder = m_motor.getEncoder();
     private final SparkPIDController m_pidController = m_motor.getPIDController(); 
-    private final ArmFeedForward feedForward = new ArmFeedForward(0, 0, 0); 
+    private final ArmFeedforward feedForward = new ArmFeedforward(0, 0, 0); 
     private double m_setPoint; 
     
     public ArmSubsystem() {
@@ -36,15 +32,16 @@ public class ArmSubsystem extends SubsystemBase {
         m_pidController.setIZone(ArmConstants.kIz);
         m_pidController.setD(ArmConstants.kD);
         m_pidController.setFF(ArmConstants.kFF);
-        
-        m_encoder.reset(); // why isnt this working?
+
+        //m_encoder.setPosition(0);
         m_encoder.setPosition(-ArmConstants.kMinPosition); 
-        
     }
     
     public void periodic() {
         
     }
 
-    
+    public void setSpeed(double speed) {
+        m_motor.set(speed);
+    }
 }
